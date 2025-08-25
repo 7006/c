80d41332fc445e0477c1b4e3f46c7ef918ece3b1
7006 <22399553+7006@users.noreply.github.com>
2025-08-25 23:48:44 +0000

    struct

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#define NAME_LEN 25
 
-int main(void) {
-  printf("Hello World\n");
+struct {
+  int number;
+  char name[NAME_LEN + 1];
+  int on_hand;
+} part1 = { 5280, "Disk drive", 10 },
+  part2 = { 9140, "Printer", 5 },
+  part3 = { .number = 7529, .name = "Floppy disk", .on_hand = 100 };
+
+int main() {
+  part1.number = 2580;
+  part1.on_hand++;;
+
+  printf("Part number: %d\n", part1.number);
+  printf("Part name: %s\n", part1.name);
+  printf("Quantity on hand: %d\n", part1.on_hand);
+
+  printf("Enter part1 quantity on_hand: ");
+  scanf("%d", &part1.on_hand);
+  printf("Quantity on hand: %d\n", part1.on_hand);
+
   return 0;
 }
