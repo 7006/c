7d183225faa758fda139093f22b2f1796723bc2a
7006 <22399553+7006@users.noreply.github.com>
2025-08-26 00:10:05 +0000

    arrow

--- main.c
+++ main.c
@@ -7,9 +7,9 @@ typedef struct {
 } Part;
 
 void print_part(Part *p) {
-  printf("Part number: %d\n", (*p).number);
-  printf("Part name: %s\n", (*p).name);
-  printf("Quantity on hand: %d\n", (*p).on_hand);
+  printf("Part number: %d\n", p->number);
+  printf("Part name: %s\n", p->name);
+  printf("Quantity on hand: %d\n", p->on_hand);
   printf("\n");
 };
 
