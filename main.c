666b6087d6f5f99f6866cef8114a1a0e6bf62592
7006 <22399553+7006@users.noreply.github.com>
2025-08-26 00:09:13 +0000

    pass pointer

--- main.c
+++ main.c
@@ -1,30 +1,30 @@
 #define NAME_LEN 25
 
-struct part {
+typedef struct {
   int number;
   char name[NAME_LEN + 1];
   int on_hand;
-};
+} Part;
 
-void print_part(struct part p) {
-  printf("Part number: %d\n", p.number);
-  printf("Part name: %s\n", p.name);
-  printf("Quantity on hand: %d\n", p.on_hand);
+void print_part(Part *p) {
+  printf("Part number: %d\n", (*p).number);
+  printf("Part name: %s\n", (*p).name);
+  printf("Quantity on hand: %d\n", (*p).on_hand);
   printf("\n");
 };
 
 int main() {
-  struct part part1 = { 5280, "Disk drive", 10 };
-  struct part part2 = { 9140, "Printer", 5 };
-  struct part part3 = {
+  Part part1 = { 5280, "Disk drive", 10 };
+  Part part2 = { 9140, "Printer", 5 };
+  Part part3 = {
     .number = 7529,
     .name = "Floppy disk",
     .on_hand = 100
   };
 
-  print_part(part1);
-  print_part(part2);
-  print_part(part3);
+  print_part(&part1);
+  print_part(&part2);
+  print_part(&part3);
 
   return 0;
 }
